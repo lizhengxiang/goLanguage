c50dc23a65bca61ca8b4bc679adf03273d98f53f
lizhengxiang <1065482100.com>
2017-12-20 16:39:35 +0800

    echo2

---
+++ echo2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main(){
+	//变量在申明的时候会初始化
+	s, sep :="",""
+	// range 每次迭代产生一对值,索引以及该索引的值
+	for _, arg := range os.Args[1:]{
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
