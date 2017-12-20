1b24d8664e32b75581496c4b664f1ea21c8d0d98
lizhengxiang <1065482100.com>
2017-12-20 16:27:39 +0800

    echo1

---
+++ echo.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main(){
+	//变量在申明的时候会初始化
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i];
+		sep = " "
+	}
+	fmt.Println(s)
+}
